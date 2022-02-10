012bddf64fe56950f7f75829645c8cb4a3a888b7
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2022-02-10 17:58:51 +0100

    Update IISLogMonitor.ps1

--- IISLogMonitor.ps1
+++ IISLogMonitor.ps1
@@ -4,8 +4,6 @@
   Get al unique endpoints and currently logged on user accessing and IIS webserver
 .DESCRIPTION
   This script parses IIS logs continuously and produces a list of unique IP. That IP is then used to gather hostname and currently logged on user via Remote Registry.
-.PARAMETER <Parameter_Name>
-    None
 .INPUTS
   IIS Logfiles
 .OUTPUTS
@@ -22,182 +20,180 @@
 #Initialize variables
 $TempFile = $env:temp +"\WebLog.csv"
 $Logpath = "C:\inetpub\logs\LogFiles\W3SVC1"
-$Computergroup = "C_Computers_WrongCTXConfig"
-$Usergroup = "C_Users_WrongCTXConfig"
+$Computergroup = "SomeADGroupForComputers"
+$Usergroup = "SomeADGroupForUsers"
 $LastIPList = ""
 $LastNumberOfIP = 0
 
 Function Ping([string]$hostname, [int]$timeout = 500, [int]$retries = 3)
 {
-$result = $true
-$ping = new-object System.Net.NetworkInformation.Ping #creates a ping object
-$i = 0
-do {
-    $i++
-	try
-    {
-      #write-host "ping"
-        $result = $ping.send($hostname, $timeout).Status.ToString()
-    }
-    catch
-    {
-			#Write-Host "error"
-			continue
-		}
-		if ($result -eq "success") { return $true }
+  $result = $true
+  $ping = new-object System.Net.NetworkInformation.Ping #creates a ping object
+  $i = 0
+  do {
+        $i++
+	       try
+         {
+           #write-host "ping"
+           $result = $ping.send($hostname, $timeout).Status.ToString()
+         }
+         catch
+         {
+           #Write-Host "error"
+           continue
+         }
+         if ($result -eq "success")
+         {
+           return $true
+         }
 
-    } until ($i -eq $retries)
-    return $false
+     } until ($i -eq $retries)
+     return $false
 }
 
 while ($true)
 {
-    # Get Current IIS log filename
-    $date = get-date
-    $year = ($date.Year).ToString()
-    $shortyear = $year.substring($year.length - 2)
-    $month = ($date.Month).ToString()
-    if ($month.Length -eq 1)
+  # Get Current IIS log filename
+  $date = get-date
+  $year = ($date.Year).ToString()
+  $shortyear = $year.substring($year.length - 2)
+  $month = ($date.Month).ToString()
+  if ($month.Length -eq 1)
+  {
+    $month = "0"+$month
+  }
+  $day = ($date.Day).ToString()
+  if ($day.Length -eq 1)
+  {
+    $day = "0"+$day
+  }
+
+  $filename = $logpath+"\u_ex"+$shortyear+$month+$day+".log"
+
+  # Find the right AD Domain Controller
+  $dc = Get-ADDomainController -DomainName $SelectedDomain -Discover -NextClosestSite
+
+  # Clear temp file
+  $TempFileExists = Test-Path $TempFile
+  If ($TempFileExists -eq $True)
+  {
+    Remove-Item $TempFile
+  }
+
+  # Process current logfile
+  (Get-Content $filename | Where-Object {$_ -notlike "#[S,V,D]*"}) -replace "#Fields: ","" | Out-File -append $TempFile
+
+  # Import the temporary CSV file to memory
+  $webLog = Import-Csv $TempFile -Delimiter " "
+
+  # Extracting all unique IP's
+  Write-Host -ForegroundColor Yellow "Gathering IP addresses..."
+  $IPList = $weblog | Select-Object -Property 'c-ip' -Unique | Sort-Object -Property 'c-ip' -Descending
+  if ($IPList.count -eq $LastNumberOfIP)
+  {
+    Write-Host "No new IP addresses detected."
+    Continue
+  }
+
+  # Use IP addresses to get hostname en username
+  Write-Host -ForegroundColor Yellow "Processing IP addresses..."
+  Foreach ($IP in $IPList)
+  {
+    # Skip localhost
+    if (($IP.'c-ip' -eq 'c-ip') -or ($IP.'c-ip' -eq "127.0.0.1") -or ($IP.'c-ip' -eq "::1"))
     {
-        $month = "0"+$month
+      continue
     }
-    $day = ($date.Day).ToString()
-    if ($day.Length -eq 1)
+
+    # Empty variables
+    $RegLM = ""
+    $RegKeyLM = ""
+    $RegKeyLM2 = ""
+    $Computername = ""
+    $User = ""
+    $aduser = ""
+    $IPOnline = $false
+
+    Write-Host "Processing IP:"$IP.'c-ip'
+    if ($LastIPList.'c-ip' -contains($ip.'c-ip'))
     {
-        $day = "0"+$day
+      Write-Host "IP:"$IP.'c-ip'"already processed in previous run. Skipping..." -ForegroundColor DarkYellow
+      Continue
     }
 
-    $filename = $logpath+"\u_ex"+$shortyear+$month+$day+".log"
-
-    # Find the right AD Domain Controller
-    $dc = Get-ADDomainController -DomainName $SelectedDomain -Discover -NextClosestSite
-
-    # Clear temp file
-    $TempFileExists = Test-Path $TempFile
-    If ($TempFileExists -eq $True)
+    $IPonline = Ping $IP.'c-ip' 100
+    if ($IPonline -eq $True)
     {
-        Remove-Item $TempFile
+      Try
+      {
+        $RegLM = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $IP.'c-ip')
+      }
+      Catch
+      {
+        Write-Host "Error accessing remote registry for IP:"$ip.'c-ip' -ForegroundColor red
+      }
     }
 
-    # Process current logfile
-    (Get-Content $filename | Where-Object {$_ -notlike "#[S,V,D]*"}) -replace "#Fields: ","" | Out-File -append $TempFile
-
-    # Import the temporary CSV file to memory
-    $webLog = Import-Csv $TempFile -Delimiter " "
-
-    # Extracting all unique IP's
-    Write-Host -ForegroundColor Yellow "Gathering IP addresses..."
-    $IPList = $weblog | Select-Object -Property 'c-ip' -Unique | Sort-Object -Property 'c-ip' -Descending
-    if ($IPList.count -eq $LastNumberOfIP)
+    if ($RegLM -ne "")
     {
-        Write-Host "No new IP addresses detected."
-        Continue
-    }
+      Try
+      {
+        $RegKeyLM = $RegLM.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName")
+      }
+      Catch
+      {
+        Write-Host "Error accessing remote registry for IP:"$ip.'c-ip' -ForegroundColor red
+      }
 
-    # Use IP addresses to get hostname en username
-    Write-Host -ForegroundColor Yellow "Processing IP addresses..."
-    Foreach ($IP in $IPList)
-    {
-        # Skip localhost
-        if (($IP.'c-ip' -eq 'c-ip') -or ($IP.'c-ip' -eq "127.0.0.1") -or ($IP.'c-ip' -eq "::1"))
+      if ($RegKeyLM -ne "")
+      {
+        $Computername = $RegKeyLM.GetValue("ComputerName")
+        Try
         {
-            continue
+          Add-ADGroupMember -Identity $Computergroup -Members (get-adcomputer $Computername) -Server $dc.HostName[0]
+          Write-Host "Computer"$Computername "added to AD group" -ForegroundColor Green
+        }
+        Catch
+        {
+          write-host "Error adding computer object to AD group" -ForegroundColor Red
         }
 
-        # Empty variables
-        $RegLM = ""
-        $RegKeyLM = ""
-        $RegKeyLM2 = ""
-        $Computername = ""
-        $User = ""
-        $aduser = ""
-        $IPOnline = $false
-
-        Write-Host "Processing IP:"$IP.'c-ip'
-        if ($LastIPList.'c-ip' -contains($ip.'c-ip'))
+        Try
         {
-            Write-Host "IP:"$IP.'c-ip'"already processed in previous run. Skipping..." -ForegroundColor DarkYellow
-            Continue
+          $RegKeyLM2 = $RegLM.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI")
+        }
+        Catch
+        {
+          Write-Host "Error accessing remote registry for IP:"$ip.'c-ip' -ForegroundColor red
         }
 
-        $IPonline = Ping $IP.'c-ip' 100
-        if ($IPonline -eq $True)
+        if ($RegKeyLM2 -ne "")
         {
+          $User = $RegKeyLM2.GetValue("LastLoggedOnUser")
+
+          if ($user -ne $null)
+          {
+            $aduser = $user.Split("\")
+            $aduser = $aduser[1]
+
             Try
             {
-                $RegLM = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $IP.'c-ip')
+              Add-ADGroupMember -Identity $usergroup -Members (get-aduser $aduser) -Server $dc.HostName[0]
+              write-host "User"$aduser "added to AD group" -ForegroundColor Green
             }
             Catch
             {
-                Write-Host "Error accessing remote registry for IP:"$ip.'c-ip' -ForegroundColor red
+              write-host "Error adding user object to AD group" -ForegroundColor Red
             }
+          }
         }
-
-        if ($RegLM -ne "")
-        {
-            Try
-            {
-                $RegKeyLM = $RegLM.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName")
-            }
-            Catch
-            {
-                Write-Host "Error accessing remote registry for IP:"$ip.'c-ip' -ForegroundColor red
-            }
-
-            if ($RegKeyLM -ne "")
-            {
-                $Computername = $RegKeyLM.GetValue("ComputerName")
-                Try
-                {
-                    Add-ADGroupMember -Identity $Computergroup -Members (get-adcomputer $Computername) -Server $dc.HostName[0]
-                    Write-Host "Computer"$Computername "added to AD group" -ForegroundColor Green
-                }
-                Catch
-                {
-                    write-host "Error adding computer object to AD group" -ForegroundColor Red
-                }
-
-                Try
-                {
-                    $RegKeyLM2 = $RegLM.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI")
-                }
-                Catch
-                {
-
-                }
-
-                if ($RegKeyLM2 -ne "")
-                {
-                    $User = $RegKeyLM2.GetValue("LastLoggedOnUser")
-
-                    if ($user -ne $null)
-                    {
-                        $aduser = $user.Split("\")
-                        $aduser = $aduser[1]
-
-                        Try
-                        {
-                            Add-ADGroupMember -Identity $usergroup -Members (get-aduser $aduser) -Server $dc.HostName[0]
-                             write-host "User"$aduser "added to AD group" -ForegroundColor Green
-                        }
-                        Catch
-                        {
-                            write-host "Error adding user object to AD group" -ForegroundColor Red
-                        }
-                    }
-                }
-
-
-
-
-        }
-
+      }
     }
-    }
+  }
 
-$LastIPList = $IPList
-$LastNumberOfIP = $LastIPList.count
+  $LastIPList = $IPList
+  $LastNumberOfIP = $LastIPList.count
 
-# Memory clean up
-[System.GC]::Collect()
+  # Memory clean up
+  [System.GC]::Collect()
 }
